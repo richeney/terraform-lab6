71205867dcb65f30b3b7895da610665d355e51f4
Richard Cheney <richard.cheney@microsoft.com>
2018-09-13 16:08:41 +0100

    Reworked NSGs

--- nsgs.tf
+++ nsgs.tf
@@ -1,11 +1,11 @@
-resource "azurerm_resource_group" "nsgs" {
+*/resource "azurerm_resource_group" "nsgs" {
    name         = "NSGs"
    location     = "${var.loc}"
    tags         = "${var.tags}"
 }
 
-resource "azurerm_network_security_group" "AllowSSH" {
-   name = "AllowSSH"
+resource "azurerm_network_security_group" "resource_group_default" {
+   name = "ResourceGroupDefault"
    resource_group_name  = "${azurerm_resource_group.nsgs.name}"
    location             = "${azurerm_resource_group.nsgs.location}"
    tags                 = "${azurerm_resource_group.nsgs.tags}"
@@ -14,9 +14,9 @@ resource "azurerm_network_security_group" "AllowSSH" {
 resource "azurerm_network_security_rule" "AllowSSH" {
     name = "AllowSSH"
     resource_group_name         = "${azurerm_resource_group.nsgs.name}"
-    network_security_group_name = "${azurerm_network_security_group.AllowSSH.name}"
+    network_security_group_name = "${azurerm_network_security_group.resource_group_default.name}"
 
-    priority                    = 1001
+    priority                    = 1010
     access                      = "Allow"
     direction                   = "Inbound"
     protocol                    = "Tcp"
@@ -26,19 +26,12 @@ resource "azurerm_network_security_rule" "AllowSSH" {
     source_address_prefix       = "*"
 }
 
-resource "azurerm_network_security_group" "AllowHTTP" {
-   name = "AllowHTTP"
-   resource_group_name  = "${azurerm_resource_group.nsgs.name}"
-   location             = "${azurerm_resource_group.nsgs.location}"
-   tags                 = "${azurerm_resource_group.nsgs.tags}"
-}
-
 resource "azurerm_network_security_rule" "AllowHTTP" {
     name = "AllowHTTP"
     resource_group_name         = "${azurerm_resource_group.nsgs.name}"
-    network_security_group_name = "${azurerm_network_security_group.AllowHTTP.name}"
+    network_security_group_name = "${azurerm_network_security_group.resource_group_default.name}"
 
-    priority                    = 1001
+    priority                    = 1020
     access                      = "Allow"
     direction                   = "Inbound"
     protocol                    = "Tcp"
@@ -48,19 +41,13 @@ resource "azurerm_network_security_rule" "AllowHTTP" {
     source_address_prefix       = "*"
 }
 
-resource "azurerm_network_security_group" "AllowHTTPS" {
-   name = "AllowHTTPS"
-   resource_group_name  = "${azurerm_resource_group.nsgs.name}"
-   location             = "${azurerm_resource_group.nsgs.location}"
-   tags                 = "${azurerm_resource_group.nsgs.tags}"
-}
 
 resource "azurerm_network_security_rule" "AllowHTTPS" {
     name = "AllowHTTPS"
     resource_group_name         = "${azurerm_resource_group.nsgs.name}"
-    network_security_group_name = "${azurerm_network_security_group.AllowHTTPS.name}"
+    network_security_group_name = "${azurerm_network_security_group.resource_group_default.name}"
 
-    priority                    = 1001
+    priority                    = 1021
     access                      = "Allow"
     direction                   = "Inbound"
     protocol                    = "Tcp"
@@ -69,19 +56,13 @@ resource "azurerm_network_security_rule" "AllowHTTPS" {
     source_port_range           = "*"
     source_address_prefix       = "*"
 }
-resource "azurerm_network_security_group" "AllowSQLServer" {
-   name = "AllowSQLServer"
-   resource_group_name  = "${azurerm_resource_group.nsgs.name}"
-   location             = "${azurerm_resource_group.nsgs.location}"
-   tags                 = "${azurerm_resource_group.nsgs.tags}"
-}
 
 resource "azurerm_network_security_rule" "AllowSQLServer" {
     name = "AllowSQLServer"
     resource_group_name         = "${azurerm_resource_group.nsgs.name}"
-    network_security_group_name = "${azurerm_network_security_group.AllowSQLServer.name}"
+    network_security_group_name = "${azurerm_network_security_group.resource_group_default.name}"
 
-    priority                    = 1001
+    priority                    = 1030
     access                      = "Allow"
     direction                   = "Inbound"
     protocol                    = "Tcp"
@@ -90,19 +71,13 @@ resource "azurerm_network_security_rule" "AllowSQLServer" {
     source_port_range           = "*"
     source_address_prefix       = "*"
 }
-resource "azurerm_network_security_group" "AllowRDP" {
-   name = "AllowRDP"
-   resource_group_name  = "${azurerm_resource_group.nsgs.name}"
-   location             = "${azurerm_resource_group.nsgs.location}"
-   tags                 = "${azurerm_resource_group.nsgs.tags}"
-}
 
 resource "azurerm_network_security_rule" "AllowRDP" {
     name = "AllowRDP"
     resource_group_name         = "${azurerm_resource_group.nsgs.name}"
-    network_security_group_name = "${azurerm_network_security_group.AllowRDP.name}"
+    network_security_group_name = "${azurerm_network_security_group.resource_group_default.name}"
 
-    priority                    = 1001
+    priority                    = 1040
     access                      = "Allow"
     direction                   = "Inbound"
     protocol                    = "Tcp"
@@ -110,4 +85,42 @@ resource "azurerm_network_security_rule" "AllowRDP" {
     destination_address_prefix  = "*"
     source_port_range           = "*"
     source_address_prefix       = "*"
+}
+
+resource "azurerm_network_security_group" "nic_ubuntu" {
+   name = "NIC_Ubuntu"
+   resource_group_name  = "${azurerm_resource_group.nsgs.name}"
+   location             = "${azurerm_resource_group.nsgs.location}"
+   tags                 = "${azurerm_resource_group.nsgs.tags}"
+
+    security_rule {
+        name                       = "SSH"
+        priority                   = 100
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = 22
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_network_security_group" "nic_windows" {
+   name = "NIC_Windows"
+   resource_group_name  = "${azurerm_resource_group.nsgs.name}"
+   location             = "${azurerm_resource_group.nsgs.location}"
+   tags                 = "${azurerm_resource_group.nsgs.tags}"
+
+    security_rule {
+        name                       = "RDP"
+        priority                   = 100
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = 3389
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+  }
 }
